df94d0e13950de4f62ae2dcb47b7c0edf5eeb420
Arina Gracheva <arinee.msc@gmail.com>
2023-11-15 15:19:04 +0300

    hw2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
-const server = http.createServer((request, response) => {
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const query = parsedUrl.query;
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+  if (parsedUrl.pathname === '/') {
+    if (query.hello) {
+      if (query.hello === '') {
+        res.writeHead(400);
+        res.end('Enter a name');
+      } else {
+        res.writeHead(200);
+        res.end(Hello, ${query.hello}!);
+      }
+    } else if (query.users) {
+      fs.readFile('data/users.json', 'utf8', (err, data) => {
+        if (err) {
+          res.writeHead(500);
+          res.end();
+        } else {
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(data);
+        }
+      });
+    } else {
+      res.writeHead(200);
+      res.end('Hello, World!');
+    }
+  } else {
+    res.writeHead(500);
+    res.end();
+  }
+});
 
+server.listen(3003, '127.0.0.1', () => {
+  console.log('Server is running at http://127.0.0.1:3003/');
 });
